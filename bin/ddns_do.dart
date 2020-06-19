f0152aeaf29a984a2e46770dc53d87a94f70b019
David Langheiter <david@langheiter.com>
2020-06-19 17:15:37 +0200

    Init commit

---
+++ bin/ddns_do.dart
@@ -0,0 +1,5 @@
+import 'package:ddns_do/ddns_do.dart' as ddns_do;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ddns_do.calculate()}!');
+}
---
+++ test/ddns_do_test.dart
@@ -0,0 +1,8 @@
+import 'package:ddns_do/ddns_do.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
