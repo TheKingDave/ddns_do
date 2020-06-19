04ffdc7cb9156d1501875c6e536b252414683869
David Langheiter <david@langheiter.com>
2020-06-19 18:53:31 +0200

    Simple testing setup

--- lib/ddns_do.dart
+++ lib/ddns_do.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ddns_do/httpError.dart';
+import 'package:ddns_do/config.dart';
+
+class DDNS {
+  final Config config;
+
+  const DDNS(this.config);
+
+  void handleRequest(HttpRequest request) {
+    if (request.method != 'GET') {
+      throw HttpError('Unsupported request: ${request.method}', 400);
+    }
+
+    final params = request.uri.queryParameters;
+    // ipv4, username, password, domain
+    // https://services.thekingdave.com/ddns/?ip=<ipaddr>&domain=<domain>&user=<usernam>&pw=<pass>
+    final remoteIp = request.connectionInfo.remoteAddress.address;
+    
+    final ip = params['ip'];
+    final domain = params['domain'];
+    final user = params['user'];
+    final password = params['pw'];
+
+    request.response
+      ..statusCode = 200
+      ..headers.add(HttpHeaders.contentTypeHeader, 'text/plain')
+      ..write(json.encode({
+        'remoteIp': remoteIp,
+        'ip': ip,
+        'domain': domain,
+        'user': user,
+        'pw': password
+      }))
+      ..close();
+  }
 }
